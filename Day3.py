426a78c4aa83fe7fa90864afffd5ff37243fea82
AluEne1 <enealu.e@gmail.com>
2024-05-27 19:29:15 +0000

    Lists

---
+++ Day3.py
@@ -0,0 +1,33 @@
+# Make a list
+
+my_list =[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+new_list = []
+newlist_user = []
+# write a program that prints out all the elements of the list that are less than 5.
+for element in my_list:
+    print(element)
+
+
+# Instead of printing the elements one by one, 
+# make a new list that has all the elements less than 5 from this list in it and print out this new list.
+
+for element in my_list:  # This code is producing results for each iteration instead of one final result
+    if element > 5:
+        new_list.append(element)
+        print(new_list)
+
+print([ element for element in my_list if element > 5]) # Here is the solution
+
+# Write this in one line of Python.
+# Ask the user for a number  
+
+user_number = int(input("Kindly input a number of your choice: "))
+
+# return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
+for element in my_list:
+    if element > user_number:
+        newlist_user.append(element)
+        print(newlist_user)
+
+print([element for element in my_list if element > user_number])
