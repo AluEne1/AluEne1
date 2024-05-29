963d0fc1e1ea08ed4f711968898754708c733add
AluEne1 <enealu.e@gmail.com>
2024-05-29 19:04:52 +0000

    Divisors

--- Day4.py
+++ Day4.py
@@ -4,10 +4,8 @@ num = int(input("Kindly input a number of your choice: ")) # get input
 num_list = range(1, num+1) 
 div_list = [] # store divisors
 
-
-
-for elem in num_list:
-    if num % elem == 0:
-        div_list.append(elem)
+for elem in num_list: # for each element in the num_list
+    if num % elem == 0: # Divide num by it if remainder = 0
+        div_list.append(elem) # add to div_list
 
 print(str(div_list) + " these are all the divisors of " + str(num))
